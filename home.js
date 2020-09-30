38b5f1c13065e69a25a15b41cc93aea08710f7b0
NaHyun Kwon <35582991+NinanoKwon@users.noreply.github.com>
2020-09-30 21:38:08 +0900

    how to manipulate DOM with javascript

---
+++ home.js
@@ -0,0 +1,16 @@
+console.log("hello");
+
+//alert("hello this is cozy");
+
+//How to write a comment inline
+
+//Variables
+var b = "smoothie";
+console.log(b);
+
+var someNumber = 45;
+console.log(someNumber);
+
+var age = prompt("What is your age?");
+
+document.getElementById("someText").innerHTML = age;
