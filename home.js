320d9c53962e2b50138f9a98fca7f00af67aee22
NaHyun Kwon <35582991+NinanoKwon@users.noreply.github.com>
2020-09-30 21:52:33 +0900

    numbers in javascript

--- home.js
+++ home.js
@@ -9,8 +9,26 @@ var b = "smoothie";
 console.log(b);
 
 var someNumber = 45;
-console.log(someNumber);
 
-var age = prompt("What is your age?");
+//console.log(someNumber);
 
-document.getElementById("someText").innerHTML = age;
+//var age = prompt("What is your age?");
+
+//document.getElementById("someText").innerHTML = age;
+
+// Numbers in javasscript
+var num1 = 10;
+
+// Increment num1 by 1
+num1++;
+
+// Decrement num1 by 1
+num1--;
+console.log(num1);
+
+// Divide, multiply *, remainder %
+console.log(num1 % 5);
+
+// Increment/decrement by 10
+num1 += 10;
+console.log(num1);
